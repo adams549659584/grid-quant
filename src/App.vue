753993e726807353f596fb9ed60d7684fe7f2390
罗君 <13760614423@163.com>
2022-02-09 21:02:34 +0800

    fix: css 调整

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,9 @@ const historyFillRowCount = computed(() => {
   }
   const nowCount = historyRows.value.length;
   const screenWidth = window.screen.width;
-  if (screenWidth >= 1280) {
+  if (screenWidth >= 1536) {
+    return 5 - (nowCount % 5);
+  } else if (screenWidth >= 1280) {
     return 4 - (nowCount % 4);
   } else if (screenWidth >= 1024) {
     return 3 - (nowCount % 3);
@@ -200,8 +202,7 @@ const backtesting = async (secid: string) => {
         holdCount.value = mathRound(holdCount.value + buyCount);
         totalMoney.value = mathRound(totalMoney.value - money);
         backtestingLogs.value.push(
-          `${kline.dateStr} ${kline.timeStr}： 低于最小持仓，￥${kline.openPrice} 买入 ${buyCount} 股 , 支出 ￥${money} ， 持仓 ${holdCount.value} ， 余额 ￥${
-            totalMoney.value
+          `${kline.dateStr} ${kline.timeStr}： 低于最小持仓，￥${kline.openPrice} 买入 ${buyCount} 股 , 支出 ￥${money} ， 持仓 ${holdCount.value} ， 余额 ￥${totalMoney.value
           } ， 市值 ￥${(kline.closePrice * holdCount.value + totalMoney.value).toFixed(2)}`
         );
       }
@@ -214,8 +215,7 @@ const backtesting = async (secid: string) => {
           holdCount.value = mathRound(holdCount.value - saleCount);
           totalMoney.value = mathRound(totalMoney.value + money);
           backtestingLogs.value.push(
-            `${kline.dateStr} ${kline.timeStr}：￥${kline.openPrice} 止盈卖出 ${saleCount} 股 , 收入 ￥${money} ， 持仓 ${holdCount.value} ，  余额 ￥${
-              totalMoney.value
+            `${kline.dateStr} ${kline.timeStr}：￥${kline.openPrice} 止盈卖出 ${saleCount} 股 , 收入 ￥${money} ， 持仓 ${holdCount.value} ，  余额 ￥${totalMoney.value
             } ， 市值 ￥${(kline.openPrice * holdCount.value + totalMoney.value).toFixed(2)}`
           );
         } else if (holdCount.value > 0 && kline.openPrice < klineNextPrice.lowBuyPrice) {
@@ -225,8 +225,7 @@ const backtesting = async (secid: string) => {
           holdCount.value = mathRound(holdCount.value - saleCount);
           totalMoney.value = mathRound(totalMoney.value + money);
           backtestingLogs.value.push(
-            `${kline.dateStr} ${kline.timeStr}：￥${kline.openPrice} 止损卖出 ${saleCount} 股 , 收入 ￥${money} ， 持仓 ${holdCount.value} ，  余额 ￥${
-              totalMoney.value
+            `${kline.dateStr} ${kline.timeStr}：￥${kline.openPrice} 止损卖出 ${saleCount} 股 , 收入 ￥${money} ， 持仓 ${holdCount.value} ，  余额 ￥${totalMoney.value
             } ， 市值 ￥${(kline.openPrice * holdCount.value + totalMoney.value).toFixed(2)}`
           );
         } else if (klineRate > 0 && holdCount.value - gridCount.value >= minHoldCount.value) {
@@ -236,8 +235,7 @@ const backtesting = async (secid: string) => {
           holdCount.value = mathRound(holdCount.value - saleCount);
           totalMoney.value = mathRound(totalMoney.value + money);
           backtestingLogs.value.push(
-            `${kline.dateStr} ${kline.timeStr}：￥${kline.openPrice} 网格卖出 ${saleCount} 股 , 收入 ￥${money} ， 持仓 ${holdCount.value} ，  余额 ￥${
-              totalMoney.value
+            `${kline.dateStr} ${kline.timeStr}：￥${kline.openPrice} 网格卖出 ${saleCount} 股 , 收入 ￥${money} ， 持仓 ${holdCount.value} ，  余额 ￥${totalMoney.value
             } ， 市值￥ ${(kline.openPrice * holdCount.value + totalMoney.value).toFixed(2)}`
           );
         } else if (klineRate < 0 && kline.openPrice > klineNextPrice.lowBuyPrice) {
@@ -256,8 +254,7 @@ const backtesting = async (secid: string) => {
           holdCount.value = mathRound(holdCount.value + buyCount);
           totalMoney.value = mathRound(totalMoney.value - money);
           backtestingLogs.value.push(
-            `${kline.dateStr} ${kline.timeStr}：￥${kline.openPrice} 网格买入 ${buyCount} 股 , 支出 ￥${money} ， 持仓 ${holdCount.value} ，  余额 ￥${
-              totalMoney.value
+            `${kline.dateStr} ${kline.timeStr}：￥${kline.openPrice} 网格买入 ${buyCount} 股 , 支出 ￥${money} ， 持仓 ${holdCount.value} ，  余额 ￥${totalMoney.value
             } ， 市值￥ ${(kline.openPrice * holdCount.value + totalMoney.value).toFixed(2)}`
           );
         }
@@ -280,8 +277,47 @@ const initNextPriceList = async () => {
   if (historyRows.value.length > 0) {
     historyRows.value.forEach((row) => calcNext(`${row.market}.${row.code}`));
   } else {
-    // '510050','159602','561990','510500','512100','516970','159928','512670','516110','512000','512480','515790','159867','159790','512760','516100','159997','159755','512980','159828','159883','513050','513330','159967','159745','512200','159825','159996','159992','515250','516780','511220','511260','159905','511380','513500','161834','501022','159981','512890'
-    ['1.000001', '1.000300', '1.000905', '0.399006'].forEach((secid) => calcNext(secid));
+    ['1.510050',// 上证50ETF
+      '0.159602',// 中国A50ETF
+      '1.561990',// 沪深300增强ETF
+      '1.510500',// 中证500ETF
+      '1.512100',// 中证1000ETF
+      '1.516970',// 基建50ETF
+      '0.159928',// 消费ETF
+      '1.512670',// 国防ETF
+      '1.516110',// 汽车ETF
+      '1.512000',// 券商ETF
+      '1.512480',// 半导体ETF
+      '1.515790',// 光伏ETF
+      '0.159867',// 畜牧ETF
+      '0.159790',// 碳中和ETF
+      '1.512760',// 芯片ETF
+      '1.516100',// 互联网金融ETF
+      '0.159997',// 电子ETF
+      '0.159755',// 电池ETF
+      '1.512980',// 传媒ETF
+      '0.159828',// 医疗ETF
+      '0.159883',// 医疗器械ETF
+      '1.513050',// 中概互联网ETF
+      '1.513330',// 恒生互联网ETF
+      '0.159967',// 创成长ETF
+      '0.159745',// 建材ETF
+      '1.512200',// 房地产ETF
+      '0.159825',// 农业ETF
+      '0.159996',// 家电ETF
+      '0.159992',// 创新药ETF
+      '1.515250',// 智能汽车ETF
+      '1.516780',// 稀土ETF
+      '1.511220',// 城投债ETF
+      '1.511260',// 十年国债ETF
+      '0.159905',// 深红利ETF
+      '1.511380',// 可转债ETF
+      '1.513500',// 标普500ETF
+      '0.161834',// 银华鑫锐LOF
+      '1.501022',// 银华鑫盛LOF
+      '0.159981',// 能源化工ETF
+      '1.512890',// 红利低波ETF
+    ].forEach((secid) => calcNext(secid));
   }
   if (isTradeTime) {
     nextPriceTimer.value = setTimeout(() => {
@@ -305,14 +341,25 @@ onBeforeUnmount(() => {
     <header>
       <h1 class="flex-center">
         <span class="title">价格预测</span>
-        <a class="cursor-pointer" href="https://github.com/adams549659584/grid-quant" target="_blank">
+        <a
+          class="cursor-pointer"
+          href="https://github.com/adams549659584/grid-quant"
+          target="_blank"
+        >
           <img src="./assets/images/github.png" alt="https://github.com/adams549659584/grid-quant" />
         </a>
       </h1>
     </header>
     <main>
       <header class="flex-center space-x-4">
-        <el-switch v-if="isShowNextSwitchChange" v-model="nextSwitch" inline-prompt active-text="预" inactive-text="回" @change="nextSwitchChange" />
+        <el-switch
+          v-if="isShowNextSwitchChange"
+          v-model="nextSwitch"
+          inline-prompt
+          active-text="预"
+          inactive-text="回"
+          @change="nextSwitchChange"
+        />
         <div>
           <input
             class="input-search"
@@ -330,9 +377,7 @@ onBeforeUnmount(() => {
                 v-for="item in searchResultRows"
                 :key="`${item.MktNum}.${item.Code}_${item.Name}`"
                 @click="selectChange(`${item.MktNum}.${item.Code}`)"
-              >
-                {{ `${item.Code} ${item.Name} ${item.SecurityTypeName}` }}
-              </li>
+              >{{ `${item.Code} ${item.Name} ${item.SecurityTypeName}` }}</li>
             </ul>
             <ul class="search-hotkey" v-else-if="searchKeyword.length > 0">
               <li class="search-hotkey-items">暂无相关股票/基金</li>
@@ -345,59 +390,80 @@ onBeforeUnmount(() => {
         <!-- <el-button class="btn-backtesting"
                    size="large"
                    type="danger"
-                   @click="backtesting">回测</el-button> -->
+        @click="backtesting">回测</el-button>-->
       </header>
       <main>
         <div class="flex-center justify-around text-xl font-bold" v-if="historyRows">
           <div class="next-price-box" v-for="(row, index) in historyRows" :key="index">
             <div class="row p-2">
               <span class="flex-1 text-center truncate">{{ `${row.code} ${row.name}` }}</span>
-              <el-button class="w-min" type="primary" :icon="CaretRight" circle @click="backtesting(`${row.market}.${row.code}`)"></el-button>
+              <el-button
+                class="w-min"
+                type="primary"
+                :icon="CaretRight"
+                circle
+                @click="backtesting(`${row.market}.${row.code}`)"
+              ></el-button>
               <el-button class="w-min" type="danger" :icon="Delete" circle @click="delHistory(row)"></el-button>
             </div>
             <div class="row">
               <div class="column">操作</div>
-              <div class="column" :class="{ 'close-price-red': row.nowPrice.closePrice > (row.nextPrice.firstSalePrice + row.nextPrice.firstBuyPrice) / 2 }">
-                价格({{ row.nowPrice.closePrice.toFixed(3) }})
-              </div>
+              <div
+                class="column close-price-green"
+                :class="{ 'close-price-red': row.nowPrice.closePrice >= (row.nextPrice.firstSalePrice + row.nextPrice.firstBuyPrice) / 2 }"
+              >价格({{ row.nowPrice.closePrice.toFixed(3) }})</div>
             </div>
             <div class="row bg-red-400">
               <div class="column">极限获利位</div>
-              <div class="column">{{ row.nextPrice.highSalePrice.toFixed(3) }}(+{{ row.nextPrice.highSaleRate }}%)</div>
+              <div
+                class="column"
+              >{{ row.nextPrice.highSalePrice.toFixed(3) }}(+{{ row.nextPrice.highSaleRate }}%)</div>
             </div>
             <div class="row bg-red-300">
               <div class="column">第一压力位</div>
-              <div class="column">{{ row.nextPrice.firstSalePrice.toFixed(3) }}(+{{ row.nextPrice.firstSaleRate }}%)</div>
+              <div
+                class="column"
+              >{{ row.nextPrice.firstSalePrice.toFixed(3) }}(+{{ row.nextPrice.firstSaleRate }}%)</div>
             </div>
             <div class="row bg-green-300">
               <div class="column">第一支撑位</div>
-              <div class="column">{{ row.nextPrice.firstBuyPrice.toFixed(3) }}(-{{ row.nextPrice.firstBuyRate }}%)</div>
+              <div
+                class="column"
+              >{{ row.nextPrice.firstBuyPrice.toFixed(3) }}(-{{ row.nextPrice.firstBuyRate }}%)</div>
             </div>
             <div class="row bg-green-400">
               <div class="column">极限抄底位</div>
-              <div class="column">{{ row.nextPrice.lowBuyPrice.toFixed(3) }}(-{{ row.nextPrice.lowBuyRate }}%)</div>
+              <div
+                class="column"
+              >{{ row.nextPrice.lowBuyPrice.toFixed(3) }}(-{{ row.nextPrice.lowBuyRate }}%)</div>
             </div>
             <div class="row">
               <div class="column">振幅</div>
-              <div class="column">
-                {{ (row.nextPrice.firstSaleRate + row.nextPrice.firstBuyRate).toFixed(2) }}% - {{ (row.nextPrice.highSaleRate + row.nextPrice.lowBuyRate).toFixed(2) }}%
-              </div>
+              <div
+                class="column"
+              >{{ (row.nextPrice.firstSaleRate + row.nextPrice.firstBuyRate).toFixed(2) }}% - {{ (row.nextPrice.highSaleRate + row.nextPrice.lowBuyRate).toFixed(2) }}%</div>
             </div>
           </div>
           <div class="next-price-box invisible" v-for="i in historyFillRowCount" :key="i"></div>
-          <div v-show="isShowBacktestingLog">
-            <div>
-              <div v-for="(item, index) in backtestingLogs" :key="index">{{ item }}</div>
-              <el-button type="danger" :icon="CircleClose" @click="closeBacktesting">看完了 </el-button>
+          <div class="backtesting-log-bg" v-show="isShowBacktestingLog">
+            <div class="backtesting-log-box">
+              <div
+                class="backtesting-log-item"
+                v-for="(item, index) in backtestingLogs"
+                :key="index"
+              >{{ item }}</div>
+              <div class="flex-center p-4">
+                <el-button type="danger" :icon="CircleClose" @click="closeBacktesting">看完了</el-button>
+              </div>
             </div>
           </div>
         </div>
       </main>
     </main>
     <footer>
-      <div>
-        <p>
-          MIT Licensed | Copyright © 2022
+      <div class="p-4 text-gray-400">
+        <p class="flex-center space-x-2">
+          <span>MIT Licensed | Copyright © 2022</span>
           <a href="https://github.com/adams549659584" target="_blank">adams549659584</a>
         </p>
       </div>
@@ -421,7 +487,7 @@ onBeforeUnmount(() => {
 
 .up-arrow {
   @apply before:absolute before:p-2 before:-top-2 before:left-1/2 before:-translate-x-1/2 before:rotate-45 
-  before:before:border-t before:border-l before:bg-inherit before:z-0;
+  before:border-t before:border-l before:bg-inherit before:z-0;
 }
 
 .search-hotkey {
@@ -436,7 +502,8 @@ onBeforeUnmount(() => {
   @apply border outline-2 mt-4 w-[98%]
    md:w-[49%]
    lg:w-[32.3%]
-   xl:w-[24%];
+   xl:w-[24%] 
+   2xl:w-[19%];
 
   .row {
     @extend .flex-center;
@@ -451,6 +518,23 @@ onBeforeUnmount(() => {
       @apply border-r;
     }
     @apply text-center truncate w-1/2 p-2;
+    &.close-price-green {
+      @apply text-green-500;
+    }
+    &.close-price-red {
+      @apply text-red-500;
+    }
+  }
+}
+
+.backtesting-log-bg {
+  @apply fixed top-0 left-0 w-full h-full bg-black/60;
+  @extend .flex-center;
+  .backtesting-log-box {
+    @apply relative bg-white border rounded-sm max-w-[90%] sm:max-w-[60%] max-h-[80vh] overflow-auto text-base text-gray-500 font-thin;
+    .backtesting-log-item {
+      @apply w-full px-4 border-b leading-loose cursor-pointer hover:bg-gray-300;
+    }
   }
 }
 </style>
