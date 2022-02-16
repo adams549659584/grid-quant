09a2438da49490fddef37a1a883a92600f6f307c
罗君 <13760614423@163.com>
2022-02-16 09:08:07 +0800

    fix: :bug: fix logo src
    fix tsconfig

--- src/components/header/GridHeader.vue
+++ src/components/header/GridHeader.vue
@@ -6,7 +6,7 @@
     <h1 class="flex justify-center items-center flex-wrap">
       <span class="text-3xl font-bold p-4">价格预测</span>
       <a class="cursor-pointer" href="https://github.com/adams549659584/grid-quant" target="_blank">
-        <img src="./assets/images/github.png" alt="https://github.com/adams549659584/grid-quant" />
+        <img src="../../assets/images/github.png" alt="https://github.com/adams549659584/grid-quant" />
       </a>
     </h1>
   </header>
