1bd09b1762d2a5dbfe85ee76e48c534953770570
罗君 <13760614423@163.com>
2022-02-16 11:40:17 +0800

    refactor: ♻️ 搜索用回element ui的

--- src/components/search/StockSearch.vue
+++ src/components/search/StockSearch.vue
@@ -2,70 +2,26 @@
 import useStockSearch from './hooks/useStockSearch';
 
 const {
-  isShowSearchResult,
   searchKeyword,
   searchResultRows,
-  searchSelectedIndex,
-  changeSearchResultShow,
+  loading,
   query,
-  searchHotkeyUp,
-  searchHotkeyDown,
-  searchHotkeyEnter,
   selectChange
 } = useStockSearch();
 </script>
 
 <template>
-  <div>
-    <input
-      class="input-search"
-      type="text"
-      placeholder="请输入股票/基金代码"
-      v-model="searchKeyword"
-      @focus="changeSearchResultShow(true)"
-      @blur="changeSearchResultShow(false)"
-      @input="query"
-      @keydown.up="searchHotkeyUp"
-      @keydown.down="searchHotkeyDown"
-      @keydown.enter="searchHotkeyEnter"
-    />
-    <div class="absolute" v-show="isShowSearchResult">
-      <ul class="search-hotkey" v-if="searchResultRows && searchResultRows.length > 0">
-        <li
-          class="search-hotkey-items bg-"
-          :class="{ 'bg-gray-100': index === searchSelectedIndex }"
-          v-for="(item, index) in searchResultRows"
-          :key="`${item.MktNum}.${item.Code}_${item.Name}`"
-          @click="selectChange(`${item.MktNum}.${item.Code}`)"
-        >
-          {{ `${item.Code} ${item.Name} ${item.SecurityTypeName}` }}
-        </li>
-      </ul>
-      <ul class="search-hotkey" v-else-if="searchKeyword.length > 0">
-        <li class="search-hotkey-items">暂无相关股票/基金</li>
-      </ul>
-      <ul class="search-hotkey" v-else>
-        <li class="search-hotkey-items">输入股票/基金 编码/简拼/全拼/中文</li>
-      </ul>
-    </div>
+  <div v-if="true">
+    <el-select v-model="searchKeyword" clearable filterable remote reserve-keyword placeholder="请输入股票/基金代码" :remote-method="query" :loading="loading" @change="selectChange">
+      <el-option
+        v-for="item in searchResultRows"
+        :key="`${item.MktNum}.${item.Code}_${item.Name}`"
+        :label="`${item.Code} ${item.Name} ${item.SecurityTypeName}`"
+        :value="`${item.MktNum}.${item.Code}_${item.Name}`"
+      >
+      </el-option>
+    </el-select>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.input-search {
-  @apply border p-2 rounded-sm hover:border-blue-500 cursor-pointer focus:border-blue-500 
-  focus:outline-none;
-}
-
-.up-arrow {
-  @apply before:absolute before:block before:-top-[2rem] before:left-1/2 before:-translate-x-1/2 before:border-[1rem] before:border-transparent before:border-b-gray-100;
-}
-
-.search-hotkey {
-  @apply relative bg-white top-4 left-0 border rounded-sm z-10;
-  @extend .up-arrow;
-}
-.search-hotkey-items {
-  @apply p-2 w-48 truncate rounded-sm cursor-pointer hover:bg-gray-100;
-}
-</style>
+<style lang="scss" scoped></style>
--- src/components/search/hooks/useStockSearch.ts
+++ src/components/search/hooks/useStockSearch.ts
@@ -3,7 +3,6 @@ import { search } from '@/api/stock/stock-api';
 import usePredict from '@/components/predict/hooks/usePredict';
 import { ref } from 'vue';
 
-const isShowSearchResult = ref(false);
 /**
  * 搜索关键字
  */
@@ -15,11 +14,6 @@ const searchKeyword = ref('');
 const searchResultRows = ref<ISearchResultRow[]>();
 
 /**
- * 搜索索引
- */
-const searchSelectedIndex = ref(0);
-
-/**
  * 是否加载中
  */
 const loading = ref(false);
@@ -28,26 +22,13 @@ const secid = ref('');
 
 export default function useStockSearch() {
   /**
-   * 是否显示搜索结果
-   */
-  const changeSearchResultShow = (isShow: boolean) => {
-    if (isShow) {
-      isShowSearchResult.value = isShow;
-    } else {
-      setTimeout(() => {
-        isShowSearchResult.value = isShow;
-      }, 300);
-    }
-  };
-
-  /**
    * 查询
    */
-  const query = async () => {
-    searchSelectedIndex.value = 0;
-    if (searchKeyword.value) {
+  const query = async (queryStr: string) => {
+    console.log(`queryStr : `, queryStr);
+    if (queryStr) {
       loading.value = true;
-      const searchResults = await search(searchKeyword.value);
+      const searchResults = await search(queryStr);
       searchResultRows.value = searchResults.Data || [];
       loading.value = false;
     } else {
@@ -55,42 +36,21 @@ export default function useStockSearch() {
     }
   };
 
-  const searchHotkeyUp = (payload: KeyboardEvent) => {
-    if (searchResultRows.value && searchResultRows.value.length > 0) {
-      searchSelectedIndex.value = searchSelectedIndex.value > 0 ? searchSelectedIndex.value - 1 : searchResultRows.value.length - 1;
-    }
-    return false;
-  };
-  const searchHotkeyDown = (payload: KeyboardEvent) => {
-    if (searchResultRows.value && searchResultRows.value.length > 0) {
-      searchSelectedIndex.value = searchSelectedIndex.value < searchResultRows.value.length - 1 ? searchSelectedIndex.value + 1 : 0;
-    }
-    return false;
-  };
-  const searchHotkeyEnter = () => {
-    if (searchResultRows.value && searchResultRows.value.length > searchSelectedIndex.value) {
-      const selectResult = searchResultRows.value[searchSelectedIndex.value];
-      selectChange(`${selectResult.MktNum}.${selectResult.Code}`);
-    }
-  };
-
   const selectChange = async (val: string) => {
-    secid.value = val;
+    if (!val) {
+      return;
+    }
+    secid.value = val.split('_')[0];
     const { calcNext } = usePredict();
-    return calcNext(val, true);
+    return calcNext(secid.value, true);
   };
 
   return {
-    isShowSearchResult,
     searchKeyword,
     searchResultRows,
-    searchSelectedIndex,
     secid,
-    changeSearchResultShow,
+    loading,
     query,
-    searchHotkeyUp,
-    searchHotkeyDown,
-    searchHotkeyEnter,
     selectChange
   };
 }
