6ba3651b8deeb9ac7a6fd9aab73fc720f10a4016
罗君 <13760614423@163.com>
2022-02-19 15:16:04 +0800

    style: 🔥 移除无用引入

--- components.d.ts
+++ components.d.ts
@@ -5,7 +5,6 @@
 declare module 'vue' {
   export interface GlobalComponents {
     BacktestingLog: typeof import('./src/components/backtesting/BacktestingLog.vue')['default']
-    BuyLogo: typeof import('./src/components/icons/BuyLogo.vue')['default']
     ElAutocomplete: typeof import('element-plus/es')['ElAutocomplete']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElPopover: typeof import('element-plus/es')['ElPopover']
@@ -14,7 +13,6 @@ declare module 'vue' {
     GridFundsCalc: typeof import('./src/components/grid/GridFundsCalc.vue')['default']
     GridHeader: typeof import('./src/components/header/GridHeader.vue')['default']
     NextPriceBox: typeof import('./src/components/predict/NextPriceBox.vue')['default']
-    SaleLogo: typeof import('./src/components/icons/SaleLogo.vue')['default']
     SimpleNextPriceBox: typeof import('./src/components/predict/SimpleNextPriceBox.vue')['default']
     StockKLine: typeof import('./src/components/kline/StockKLine.vue')['default']
     StockSearch: typeof import('./src/components/search/StockSearch.vue')['default']
