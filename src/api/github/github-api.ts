f5a263e966309bfb01b4aa958e4f9c955e91d0be
罗君 <13760614423@163.com>
2022-03-03 17:58:12 +0800

    feat: 🚩 github issue api 完善

--- src/api/github/github-api.ts
+++ src/api/github/github-api.ts
@@ -1,14 +1,26 @@
 import { queryStringify } from '@/helpers/UrlHelper';
 import { IAccessTokenResult } from './model/IAccessTokenResult';
+import { IGithubUser } from './model/IGithubUser';
 
-export async function fetchGithub(url: string, accessToken: string, method: 'GET' | 'POST' = 'GET') {
+export async function fetchGithubByToken<T>(url: string, accessToken: string, method: 'GET' | 'POST' = 'GET', body?: string) {
   return fetch(url, {
     method,
     headers: {
       Accept: 'application/json',
       Authorization: `token ${accessToken}`
+    },
+    body
+  }).then((res) => res.json() as any as T);
+}
+
+export async function fetchGithubByAuth<T>(url: string, clientID: string, clientSecret: string, method: 'GET' | 'POST' = 'GET') {
+  return fetch(url, {
+    method,
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Basic ' + window.btoa(`${clientID}:${clientSecret}`)
     }
-  }).then((res) => res.json());
+  }).then((res) => res.json() as any as T);
 }
 
 export function getLoginLink(clientID: string, redirectUri = window.location.href) {
@@ -40,5 +52,30 @@ export async function getAccessToken(code: string, clientID: string, clientSecre
 
 export async function getUser(accessToken: string) {
   const url = `https://api.github.com/user`;
-  return fetchGithub(url, accessToken);
+  return fetchGithubByToken<IGithubUser>(url, accessToken);
 }
+
+export async function getIssueById(clientID: string, clientSecret: string, owner: string, repo: string, number: number) {
+  const url = `https://api.github.com/repos/${owner}/${repo}/issues/${number}`;
+  return fetchGithubByAuth(url, clientID, clientSecret, 'GET');
+}
+
+export async function getIssueByLabels(clientID: string, clientSecret: string, owner: string, repo: string, labels: string[]) {
+  const url = `https://api.github.com/repos/${owner}/${repo}/issues?labels=${labels.join(',')}`;
+  return fetchGithubByAuth(url, clientID, clientSecret, 'GET');
+}
+
+export async function createIssue(accessToken: string, owner: string, repo: string, title: string, body: string, labels: string[]) {
+  const url = `https://api.github.com/repos/${owner}/${repo}/issues`;
+  const args = { title, body, labels };
+  return fetchGithubByToken(url, accessToken, 'POST', JSON.stringify(args));
+}
+
+export async function getComments() {}
+
+export async function createComment(accessToken: string) {
+  const url = ``;
+  return fetchGithubByToken(url, accessToken);
+}
+
+// https://docs.github.com/cn/rest/reference/issues#create-an-issue
--- src/components/auth/hooks/useAuth.ts
+++ src/components/auth/hooks/useAuth.ts
@@ -1,17 +1,19 @@
-import { getAccessToken, getLoginLink, getUser } from '@/api/github/github-api';
+import * as githubApi from '@/api/github/github-api';
 import { ref } from 'vue';
 import { queryParse } from '@/helpers/UrlHelper';
 
 const CLIENT_ID = import.meta.env.PROD ? 'a49f39a4ff0992e4a4e1' : 'e8b074f95d61ad093415';
 const CLIENT_SECRETS = import.meta.env.PROD ? 'ec0a698bc1d4f5671956902ebd6b602fbddf5876' : 'e7fe47f759271189442d69a4a66592563a3fd5ea';
 const ACCESS_TOKEN_CACHE_KEY = 'access_token';
+const OWNER = 'adams549659584';
+const REPO = 'grid-quant';
 
 const isLogin = ref(false);
 
 const getAuthToken = () => localStorage.getItem(ACCESS_TOKEN_CACHE_KEY) || '';
 
 const toLogin = () => {
-  const loginUrl = getLoginLink(CLIENT_ID, window.location.href);
+  const loginUrl = githubApi.getLoginLink(CLIENT_ID, window.location.href);
   return (window.location.href = loginUrl);
 };
 const initLoginStatus = async () => {
@@ -22,7 +24,7 @@ const initLoginStatus = async () => {
   }
   const { code } = queryParse();
   if (code) {
-    const tokenResult = await getAccessToken(code, CLIENT_ID, CLIENT_SECRETS);
+    const tokenResult = await githubApi.getAccessToken(code, CLIENT_ID, CLIENT_SECRETS);
     if (tokenResult && tokenResult.access_token) {
       token = tokenResult.access_token;
       localStorage.setItem(ACCESS_TOKEN_CACHE_KEY, token);
@@ -30,8 +32,13 @@ const initLoginStatus = async () => {
     }
   }
 };
+
 const getUserInfo = async () => {
-  return getUser(getAuthToken());
+  return githubApi.getUser(getAuthToken());
+};
+
+const getIssueById = async (issueNumber: number) => {
+  return githubApi.getIssueById(CLIENT_ID, CLIENT_SECRETS, OWNER, REPO, issueNumber);
 };
 
 export default function useAuth() {
@@ -39,6 +46,7 @@ export default function useAuth() {
     isLogin,
     initLoginStatus,
     toLogin,
-    getUserInfo
+    getUserInfo,
+    getIssueById
   };
 }
