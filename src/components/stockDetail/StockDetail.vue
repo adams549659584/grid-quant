5007ca40b167e6fd53bd64b4d025b91b4e398c97
罗君 <13760614423@163.com>
2022-03-10 16:37:05 +0800

    feat: ➕ 新增K线详情

--- src/components/stockDetail/StockDetail.vue
+++ src/components/stockDetail/StockDetail.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import SvgIcon from '../icons/SvgIcon.vue';
 import useStockDetail from './hooks/useStockDetail';
-import KLineDetail from './KLineDetail.vue';
 
-const { isShowStockDetail, stockActiveInfo, klineTypes, stockActiveTab, lastStockActiveTab, tabChange, fundHoldDetails, isLoadingHoldDetails } = useStockDetail();
+const { isShowStockDetail, stockActiveInfo } = useStockDetail();
 const closeStockDetail = () => (isShowStockDetail.value = false);
-
-
-onMounted(() => { });
 </script>
 
 <template>
@@ -17,47 +12,15 @@ onMounted(() => { });
     @touchmove.prevent
     @mousewheel.prevent
   >
-    <div
-      class="w-full p-[0.5rem] md:w-[50rem] bg-white rounded-md mx-auto text-center leading-none z-20 relative"
-    >
-      <SvgIcon
-        class="w-[2rem] h-[2rem] absolute top-[0.1rem] right-[0.4rem] cursor-pointer z-3"
-        name="close"
-        color="#999"
-        @click="closeStockDetail"
-      />
-      <div class="w-full">
-        <h1
-          class="p-2 pb-4"
-          v-if="stockActiveInfo"
-        >{{ `${stockActiveInfo.code} ${stockActiveInfo.name}` }}</h1>
-        <main>
-          <el-tabs
-            type="border-card"
-            v-model="stockActiveTab"
-            class="demo-tabs"
-            @tab-click="tabChange"
-          >
-            <el-tab-pane v-for="item in klineTypes" :label="item" :name="item" lazy>
-              <div v-if="item === '持仓明细'">
-                <el-table
-                  v-loading="isLoadingHoldDetails"
-                  :data="fundHoldDetails"
-                  stripe
-                  border
-                  empty-text="暂无相关持仓数据"
-                >
-                  <el-table-column prop="code" label="代码" />
-                  <el-table-column prop="name" label="名称" />
-                  <el-table-column prop="holdPercentage" label="持仓占比" />
-                  <el-table-column prop="holdCount" label="持股数（万股）" />
-                  <el-table-column prop="holdAmt" label="持仓市值（万元）" />
-                </el-table>
-              </div>
-              <KLineDetail v-else class="w-full h-96" />
-            </el-tab-pane>
-          </el-tabs>
-        </main>
+    <div class="w-full md:max-w-7xl bg-white rounded-md mx-auto text-center leading-none z-20 relative">
+      <SvgIcon class="w-[2rem] h-[2rem] absolute top-[0.1rem] right-[0.4rem] cursor-pointer z-3" name="close" color="#999" @click="closeStockDetail" />
+      <div v-if="stockActiveInfo" class="w-full">
+        <iframe
+          :src="`//quote.eastmoney.com/basic/h5chart-iframe.html?code=${stockActiveInfo.code}&market=${stockActiveInfo.market}&type=r`"
+          width="1280px"
+          height="720px"
+          frameborder="0"
+        ></iframe>
       </div>
     </div>
   </div>
--- src/components/stockDetail/hooks/useStockDetail.ts
+++ src/components/stockDetail/hooks/useStockDetail.ts
@@ -1,49 +1,23 @@
 import { IFundHoldDetail } from '@/api/stock/model/IFundHoldDetailResult';
 import { IHistoryRow } from '@/api/stock/model/IHistoryRow';
-import { getFundHoldDetail } from '@/api/stock/stock-api';
-import { ElMessage } from 'element-plus';
+import useStockHistory from '@/components/history/hooks/useStockHistory';
 import { ref } from 'vue';
 
 const isShowStockDetail = ref(false);
-const klineTypes = ['分时', '15分', '日K', '周K', '月K', '年K', '持仓明细'] as const;
+// const klineTypes = ['分时', '15分', '日K', '周K', '月K', '年K', '持仓明细'] as const;
 // const klineTypes = ['分时', '15分', '日K', '周K', '月K', '年K'] as const;
-const stockActiveTab = ref<typeof klineTypes[number]>('分时');
+// const stockActiveTab = ref<typeof klineTypes[number]>('分时');
 const stockActiveInfo = ref<IHistoryRow>();
 
-const lastStockActiveTab = ref(stockActiveTab.value);
-const fundHoldDetails = ref<IFundHoldDetail[]>();
-const isLoadingHoldDetails = ref(false);
+// const lastStockActiveTab = ref(stockActiveTab.value);
+// const fundHoldDetails = ref<IFundHoldDetail[]>();
+// const isLoadingHoldDetails = ref(false);
 
 const showStockDetail = async (row: IHistoryRow) => {
-  stockActiveInfo.value = row;
-  // isShowStockDetail.value = true;
-  await tabChange();
-};
-
-const tabChange = async () => {
-  if (!stockActiveInfo.value) {
-    ElMessage.error('股票信息有误！');
-    return;
-  }
-  if (stockActiveTab.value === '持仓明细') {
-    isLoadingHoldDetails.value = true;
-    // 缓存
-    const cacheKey = `hold_detail_${stockActiveInfo.value.code}`;
-    const cacheDetails = sessionStorage.getItem(cacheKey);
-    if (cacheDetails) {
-      fundHoldDetails.value = JSON.parse(cacheDetails) as IFundHoldDetail[];
-    } else {
-      const fundHoldDetailResults = await getFundHoldDetail(stockActiveInfo.value.code);
-      sessionStorage.setItem(cacheKey, JSON.stringify(fundHoldDetailResults));
-      fundHoldDetails.value = fundHoldDetailResults;
-    }
-    isLoadingHoldDetails.value = false;
-  }
-  if (stockActiveTab.value === lastStockActiveTab.value) {
-    console.log(`已打开 : `, lastStockActiveTab.value);
-  } else {
-    lastStockActiveTab.value = stockActiveTab.value;
-    console.log(`切换 ： `, stockActiveTab.value);
+  const { isMobileScreen } = useStockHistory();
+  if (!isMobileScreen) {
+    stockActiveInfo.value = row;
+    isShowStockDetail.value = true;
   }
 };
 
@@ -51,12 +25,6 @@ export default function useStockDetail() {
   return {
     isShowStockDetail,
     showStockDetail,
-    stockActiveInfo,
-    klineTypes,
-    stockActiveTab,
-    lastStockActiveTab,
-    tabChange,
-    fundHoldDetails,
-    isLoadingHoldDetails
+    stockActiveInfo
   };
 }
